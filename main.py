697f0220b5cc31c9b87d0fc51cb0bc71dd9c1f43
Jefferson Guerra <jefferson@valeuapp.com>
2021-04-20 06:09:56 -0300

    Implementaçao do DSatur

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ class Vertex:
         self.degree = 0
 
     # OK
-    def addSaturationDegree(self):
-        for vertex in self.value:
-            if vertex.color != 0:
+    def calculateSaturationDegree(self):
+        for neighbor in self.neighborhood:
+            if neighbor.color != 0:
                 self.saturation_degree += 1
-        return None
+
+    # OK
+    def addSaturationDegree(self):
+        self.saturation_degree += 1
 
     # OK
     def decreaseSaturationDegree(self):
-        for vertex in self.value:
-            if vertex.color != 0:
-                self.saturation_degree -= 1
-        return None
+        self.saturation_degree -= 1
     
     # OK
     def addDegree(self):
         self.degree += 1
 
-    
+    # OK
     def addNeighborhoodSaturationDegree(self):
         for neighbor in self.neighborhood:
             neighbor.addSaturationDegree()
@@ -40,7 +40,7 @@ class Vertex:
         colors_already_used = set()
 
         for neighbor in self.neighborhood:
-            if neighbor.color == 0:
+            if neighbor == 0:
                 continue
             colors_already_used.add(neighbor.color)
         possible_colors_set -= colors_already_used
@@ -56,14 +56,17 @@ class Graph:
         self.vertices = self.generateVertices(number_of_vertices)
         self.edges = []
 
+    # OK
     def generateVertices(self, number_of_vertices):
         vertices = dict()
         for i in range(number_of_vertices):
             vertices[i + 1] = Vertex(i + 1)
         return vertices
+
+    # OK
     def addEdge(self,u,v):
-        self.vertices[u].neighborhood.append(v)
-        self.vertices[v].neighborhood.append(u)
+        self.vertices[u].neighborhood.append(self.vertices[v])
+        self.vertices[v].neighborhood.append(self.vertices[u])
         self.vertices[u].addDegree()
         self.vertices[v].addDegree()
         if (u, v) in self.edges or (v, u) in self.edges:
@@ -104,6 +107,14 @@ class Graph:
                     sigma.insert(c, r)
         return sigma
 
+    # OK
+    def todosColoridos(self):
+        for vertex in self.vertices.items():
+            if vertex[1].color == 0:
+                return False
+        return True
+
+    # OK
     def vertexOrderedDegreeDict(self):
          # Generate a dict with the degree of every vertex in the graph
         vertex_degree_dict = {vertex:vertex.degree for vertex in self.vertices.values()}
@@ -111,22 +122,58 @@ class Graph:
         # Order from the highest degree to the lowest degree
         return dict(sorted(vertex_degree_dict.items(), key=lambda x:x[1], reverse=True))
 
-    def higherSaturation (self):
+    # OK
+    def higherSaturation(self):
         higherSaturation = 0
-        vertex = Vertex()
-        for vertex in self.vertices:
-            if vertex.saturation > higherSaturation and vertex.color == 0:
-                higherSaturation = vertex.saturation
-                vertex = vertex
-        return vertex
+        higherSaturationVertex = None
+        for vertex in self.vertices.items():
+            if vertex[1].saturation_degree > higherSaturation and vertex[1].color == 0:
+                higherSaturation = vertex[1].saturation_degree
+                higherSaturationVertex = vertex[1]
+            elif vertex[1].color == 0:
+                higherSaturationVertex = vertex[1]
+        return higherSaturationVertex
 
-g = Graph(4)
-g.addEdge(1,2)
-g.addEdge(2,3)
-g.addEdge(1,4)
-print("Vertices")
-print([g.vertices])
-print("Edges")
-print(g.edges)
-print("Vertex Degree Dict")
-print(g.vertexOrderedDegreeDict())
+
+    # OK
+    def color(self):
+        if self.todosColoridos():
+            return True
+        higherSaturationVertex = self.higherSaturation()
+        possibleColorsSet = higherSaturationVertex.possibleColorsSet(len(self.vertices.items()))
+        if not possibleColorsSet:
+            return False
+        for color in possibleColorsSet:
+            higherSaturationVertex.color = color
+            higherSaturationVertex.addNeighborhoodSaturationDegree()
+            if self.color():
+                return True
+            else:
+                higherSaturationVertex.decreaseNeighborhoodSaturationDegree()
+                higherSaturationVertex.color = 0
+        return False
+
+    def printEdges(self):
+        for edge in self.edges:
+            print("({}) --> ({})".format(edge[0], edge[1]))
+        return True
+
+    def printColoredVertices(self):
+        for vertex in self.vertices.items():
+                print("Vertex ({}) with color {}".format(vertex[1].key, vertex[1].color))
+
+g = Graph(10)
+g.addEdge(1, 4)
+g.addEdge(2, 3)
+g.addEdge(2, 1)
+g.addEdge(4, 1)
+g.addEdge(4, 6)
+g.addEdge(1, 7)
+g.addEdge(9, 2)
+g.addEdge(9, 4)
+print("Colorindo")
+print(g.color())
+print("Grafo")
+print(g.printEdges())
+print("Vértices coloridos")
+print(g.printColoredVertices())
